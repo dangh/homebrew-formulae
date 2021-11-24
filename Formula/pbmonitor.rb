3f7e233a0bf7e92288dc6a7b6fe44d6914f6c130
Dang <huynhminhdang@gmail.com>
2021-11-24 13:06:05 +0800

    correct homepage url

--- Formula/pbmonitor.rb
+++ Formula/pbmonitor.rb
@@ -1,6 +1,6 @@
 class Pbmonitor < Formula
   desc "Homebrew formulae"
-  homepage "https://github.com/dangh/homebrew-formulae"
+  homepage "https://github.com/dangh/pbmonitor"
   url "https://github.com/dangh/pbmonitor/releases/download/1.0.1/pbmonitor-1.0.1.tar.gz"
   sha256 "2ad75f694c94c7a1a867a89bfa753a31142c46a487c9b44fcf4f0169829f5c35"
   license "MIT"
