9d81ee71617814434f4015260f5713aa5b31d38a
Dang <huynhminhdang@gmail.com>
2021-11-24 13:00:48 +0800

    fix tap bottle

--- Formula/pbmonitor.rb
+++ Formula/pbmonitor.rb
@@ -1,11 +1,15 @@
 class Pbmonitor < Formula
   desc "Homebrew formulae"
   homepage "https://github.com/dangh/homebrew-formulae"
-  url "https://github.com/dangh/pbmonitor/archive/refs/tags/1.0.1.tar.gz"
-  sha256 "9e32e61ec1cc7e36d442ed44c3a0b5168d21ebb90ffe25524e9a7a46996b7b62"
+  url "https://github.com/dangh/pbmonitor/releases/download/1.0.1/pbmonitor-1.0.1.tar.gz"
+  sha256 "a33affec8291fa5080c057adc7a7bf8e1f77b6f5e6c1c30aaac0786f80091b9b"
   license "MIT"
 
   def install
     bin.install "pbmonitor"
+  end
+  
+  test do
+    system "#{bin}/pbmonitor", "--help"
   end
 end
