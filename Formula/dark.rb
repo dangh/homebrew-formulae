0e474583fa5a022a60078b9f3cbc6bb32418042b
Dang <huynhminhdang@gmail.com>
2024-04-07 22:57:35 +0700

    Update dark.rb

--- Formula/dark.rb
+++ Formula/dark.rb
@@ -1,8 +1,8 @@
 class Dark < Formula
   desc "Homebrew formulae"
   homepage "https://github.com/dangh/dark"
-  url ""
-  sha256 ""
+  url "https://github.com/dangh/dark/releases/download/v0.0.1/dark"
+  sha256 "9e3b18dea9d99b6d13d1192bcbaa0a537dbe9f08d219777886602e3fe03da519"
   license "MIT"
 
   def install
